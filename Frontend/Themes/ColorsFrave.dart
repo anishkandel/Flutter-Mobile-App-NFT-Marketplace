6f2f9aa63dcaa298f8f345d8dfc4c2224ce16962
Dego <48290509+anishkandel@users.noreply.github.com>
2025-02-13 22:37:07 +0545

    themes (colors & maps) update

---
+++ Frontend/Themes/ColorsFrave.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/cupertino.dart';
+
+class ColorsFrave {
+
+  static Color primaryColor = Color(0xFFFFEB3B);
+  static Color secundaryColor = Color(0xFFFF5722);
+  static Color backgroundColor = Color(0xFF64FFDA);
+}
